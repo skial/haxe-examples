2aacdf406f9d30899ab270609a6973f7b233752b
Skial Bainn <skial.bainn@gmail.com>
2010-08-31 15:00:22 +0100

    Added descriptions, not as in depth as the tutorial

--- 2.06/metadata/pseudo-db/src/com/skialbainn/pseudo/db/Model.hx
+++ 2.06/metadata/pseudo-db/src/com/skialbainn/pseudo/db/Model.hx
@@ -5,14 +5,30 @@ import haxe.rtti.Meta;
 class Model {
 	
 	public function put() {
+		/*
+		 * We get the objects class by passing 'this' instead of
+		 * passing class Model as Model is ment to be extended.
+		 */
 		var cls = Type.getClass(this);
+		/*
+		 * We get all fields that have metadata, which we will 
+		 * check loop through to check the values are correct.
+		 */
 		var meta = Meta.getFields(cls);
-		var fields = Type.getInstanceFields(cls);
 		var _temp_;
-		
+		/*
+		 * Here we call the method checkName() passing in the class.
+		 */
 		checkName(cls);
-		
+		/*
+		 * Here we get all fields as strings which have metadata, by
+		 * using the Reflect method fields, then loop through them.
+		 */
 		for (f in Reflect.fields(meta)) {
+			/*
+			 * We get the value of the field name, which we then pass
+			 * to checkKeyRequired for further processing.
+			 */
 			_temp_ = Reflect.field(meta, f);
 			checkKeyRequired(_temp_, f);
 		}
@@ -20,22 +36,43 @@ class Model {
 	
 	// INTERNAL METHODS
 	
+	/*
+	 * This method takes the object class then which uses that
+	 * to get its metadata.
+	 */
 	private function checkName(_cls) {
+		/*
+		 * Here we are getting the metadata attached to the class.
+		 */
 		var _meta = Meta.getType(_cls);
-		var _name = Reflect.field(_meta, 'table');
-		
-		if (_name != null) {
+		/*
+		 * We then check to see if @table has a value or not. If
+		 * its true, then we continue with the pseudo db code, if
+		 * false, we raise an error describing the issue.
+		 */
+		if (_meta.table != null) {
 			// pseudo db code
 		} else {
 			trace(err(this + ' requires table name'));
 		}
 	}
 	
+	/*
+	 * Here we check for any @key and @required tags.
+	 */
 	private function checkKeyRequired(_meta, _field) {
-		var _msg_;
+		/*
+		 * If @key is found, we continue with the pseudo db code.
+		 */
 		if (has(_meta, 'key')) {
 			// pseudo db code
 		}
+		/*
+		 * If @required is found, we check to see if the value is
+		 * 'null'. If it is then we raise an error, describing
+		 * which field needs editing. If not we continue with the
+		 * pseudo db code.
+		 */
 		if (has(_meta, 'required')) {
 			if (Reflect.field(this, _field) == null) {
 				trace(err('Field ' + _field + ' cant be null'));
@@ -44,10 +81,17 @@ class Model {
 		}
 	}
 	
+	/* 
+	 * This checks the variable if it has a metadata tag.
+	 */
 	private inline function has(_meta, _field):Bool {
 		return Reflect.hasField(_meta, _field);
 	}
 	
+	/*
+	 * This returns the error message passed to it. For better 
+	 * use, replace 'return _msg' with 'throw _msg'.
+	 */
 	private inline function err(_msg):String {
 		return _msg;
 	}
